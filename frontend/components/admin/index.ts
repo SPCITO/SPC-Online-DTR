9d4b53d19b07c773a512feb86b02e540759d3932
Zham Tala <zhamtala@gmail.com>
2026-06-23 10:33:25 +0800

    vercel frontend deployment; index.ts change

--- frontend/components/admin/index.ts
+++ frontend/components/admin/index.ts
@@ -3,4 +3,5 @@ export { default as PageHeader } from "./PageHeader";
 export { default as TableHeader } from "./TableHeader";
 export { default as LogTable } from "./LogTable";
 export { default as ExportButton, exportDepartmentLogs } from "./ExportButton";
-export { BentoGrid, BentoCard } from "./BentoGrid";
+export { default as BentoGrid } from "./BentoGrid";
+export { BentoCard } from "./BentoGrid";
